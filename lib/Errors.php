2fd8a0080850906aeb81e1c67041274a32c96add
user10 <igor.baskak@gmail.com>
2017-04-17 18:22:10 +0300

    edited namespace

--- lib/Errors.php
+++ lib/Errors.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Baskakov\Multiexception;
+namespace Baskakov\MultiException;
 
 /**
  * Class Errors
--- lib/IteratorTrait.php
+++ lib/IteratorTrait.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Baskakov\Multiexception;
+namespace Baskakov\MultiException;
 
 /**
  * Trait IteratorTrait
